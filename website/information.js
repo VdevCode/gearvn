cc8555d70876fe288797a91a413621abfeafb4ea
vdevcode <thanhvumaidao1234567@gmail.com>
2022-08-28 10:19:16 +0700

    f

---
+++ website/information.js
@@ -0,0 +1,74 @@
+const buttonGrid = document.querySelector('#menu')
+const showGridMenu = document.querySelector('.menu__header')
+
+buttonGrid.addEventListener('click', function () {
+
+    buttonGrid.classList.toggle('bxs-message-alt-x')
+    if(buttonGrid.classList.contains('bxs-message-alt-x')){
+        showGridMenu.classList.toggle('active')
+    }else{
+        showGridMenu.classList.remove('active')
+    }
+})
+//darkmode
+const openDarkmode = document.querySelector('#darkmode')
+openDarkmode.addEventListener('click',function(){
+    openDarkmode.classList.toggle('bxs-moon')
+    if(openDarkmode.classList.contains('bxs-moon')){
+        document.body.classList.toggle('active')
+    }
+    else{
+        document.body.classList.remove('active')
+    }
+})
+
+const slideHeaderMenu = document.querySelector(".header");
+
+const slideHeaderVisibility = () => {
+    if (window.scrollY > 800) {
+        slideHeaderMenu.style.visibility = "hidden";
+    } 
+    else{
+        slideHeaderMenu.style.visibility = "visible";
+    }
+};
+
+document.addEventListener("scroll", () => {
+    slideHeaderVisibility();
+});
+
+//scroll top
+const scrollBtn = document.querySelector(".btn");
+
+const btnVisibility = () => {
+    if (window.scrollY > 400) {
+        scrollBtn.style.visibility = "visible";
+    } else {
+        scrollBtn.style.visibility = "hidden";
+    }
+};
+
+document.addEventListener("scroll", () => {
+    btnVisibility();
+});
+
+scrollBtn.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
+
+
+var date = new Date()
+var month = date.getMonth()
+var day = date.getDate()
+var year = date.getFullYear()
+
+const dayTime = document.querySelector('.dayTime')
+const monthTime = document.querySelector('.monthTime')
+const yearTime = document.querySelector('.yearTime')
+
+dayTime.innerHTML = day
+monthTime.innerHTML = month
+yearTime.innerHTML = year
